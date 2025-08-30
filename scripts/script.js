3fcb0f647ceffdc56c637b8bbb3a72ce404e78e2
Valerii-prog-beep <valerii_bogovich87@mail.ru>
2025-08-30 14:31:31 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,38 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const taskList = document.getElementById('taskList');
 
+addTaskBtn.addEventListener('click', addTask);
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+
+  if (taskText !== '') {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+      <input type="checkbox">
+      <span>${taskText}</span>
+      <button>Удалить</button>
+    `;
+
+    const checkbox = listItem.querySelector('input[type="checkbox"]'); // Получаем ссылку на чекбокс
+    const deleteButton = listItem.querySelector('button'); // Получаем ссылку на кнопку "Удалить"
+
+    checkbox.addEventListener('change', toggleTask); // Добавляем обработчик события для чекбокса
+    deleteButton.addEventListener('click', deleteTask); // Добавляем обработчик события для кнопки "Удалить"
+
+    taskList.appendChild(listItem);
+    taskInput.value = '';
+  }
+}
+
+function toggleTask(event) {
+  const listItem = event.target.parentNode; // Получаем родительский элемент (li)
+  listItem.classList.toggle('completed'); // Добавляем/удаляем класс "completed"
+}
+
+function deleteTask(event) {
+  console.log("Функция deleteTask вызвана");
+  const listItem = event.target.parentNode; // Получаем родительский элемент (li)
+  taskList.removeChild(listItem); // Удаляем элемент списка из списка задач
+}
